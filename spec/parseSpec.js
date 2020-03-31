6c046c9b4e9229a23222ef8e80ad46876c043093
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-03-30 19:29:34 -0600

    add jasmine unit test setup

--- spec/parseSpec.js
+++ spec/parseSpec.js
@@ -3,6 +3,5 @@ const { xmlJsonToBlog, xmlJsonToPost } = require('../helpers/parse.js');
 describe('xmlJsonToBlog', function () {
     it('produces null when there is no tumblr property', function () {
         expect(xmlJsonToBlog({})).toBe(null);
-        expect(true).toBe(false);
     });
 });
